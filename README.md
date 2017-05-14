4dd4915502627ac3271ce620fcfd46a90dccfb30
Twilight Sparkles <twi-light@users.noreply.github.com>
2017-05-14 00:08:24 -0500

    environment setup

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# sparkles
-An expirimental chat client for f-chat
+# ✨Sparkles ✨
+
+An experimental chat client for the f-chat protocol.
+
+Written in [LightScript](http://www.lightscript.org/) which is Pythonic ES7 + Flow.
+
+```bash
+npm i -g yarn
+
+yarn
+yarn start
+yarn test
+yarn build
+```
+
+MIT License
---
+++ webpack.config.babel.js
@@ -0,0 +1,83 @@
+import { resolve } from 'path'
+import Package from './package.json'
+import {
+  HotModuleReplacementPlugin,
+  NamedModulesPlugin,
+  EnvironmentPlugin
+} from 'webpack'
+import BabiliPlugin from 'babili-webpack-plugin'
+import OfflinePlugin from 'offline-plugin'
+import FlowtypePlugin from 'flowtype-loader/plugin'
+
+import eslintOptions from './.eslintrc.json'
+
+import {readFileSync} from 'fs'
+const babelOptions = JSON.parse(readFileSync(resolve(__dirname, '.babelrc')))
+
+const production = !/dev/i.test(`${process.argv}`)
+
+export default {
+  entry: { [`${Package.name}`]: `./src/${Package.name}.lsc` },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: '[name].js?[hash]',
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.lsc$/,
+      use: [{
+        loader: 'babel-loader',
+        options: babelOptions
+      }, {
+        loader: 'eslint-loader',
+        options: eslintOptions
+      },
+      { loader: 'flowtype-loader' }
+    ]
+    }, {
+      test: /\.styl/,
+      loaders: [
+        'style-loader',
+        'css-loader',
+        'stylus-loader',
+        'stylint-loader'
+      ]
+    }]
+  },
+  resolve: {
+    modules: [
+      resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.json', '.lsc', '.styl']
+  },
+  devtool: production ? 'source-map' : 'cheap-module-eval-source-map',
+  context: __dirname,
+  target: 'web',
+  devServer: {
+    contentBase: __dirname,
+    hot: true
+  },
+  plugins: production ? [
+    new EnvironmentPlugin({
+      NODE_ENV: 'production',
+      DEBUG: false
+    }),
+    new FlowtypePlugin(),
+    new OfflinePlugin({
+      externals: [
+        'index.html'
+      ],
+    }),
+    new BabiliPlugin()
+  ] : [
+    new EnvironmentPlugin({
+      NODE_ENV: 'development',
+      DEBUG: true
+    }),
+    new FlowtypePlugin(),
+    new NamedModulesPlugin(),
+    new HotModuleReplacementPlugin()
+  ]
+}
